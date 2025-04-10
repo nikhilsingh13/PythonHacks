acd8b27ed8366d4f73863d41917ef1beeb7a00c9
Nikhil Singh <nikhilsingh2009@gmail.com>
2025-04-10 21:46:08 +0200

    adding support fn linalg

---
+++ support/linear_algebra.py
@@ -0,0 +1,75 @@
+# Reference: 
+# https://github.com/joelgrus/data-science-from-scratch/blob/master/scratch/linear_algebra.py#L123C1-L132C69
+
+import math
+from typing import List, Callable
+
+Vector = List[float]
+Matrix = List[List[float]]
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)
+    """
+    return [[entry_fn(i, j)             # given i, create a list
+             for j in range(num_cols)]  #   [entry_fn(i, 0), ... ]
+            for i in range(num_rows)]   # create one list for each i
+
+def subtract(v: Vector, w: Vector) -> Vector:
+    """Subtracts corresponding elements"""
+    assert len(v) == len(w), "vectors must be the same length"
+
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be same length"
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+def squared_distance(v: Vector, w: Vector) -> float:
+    """Computes (v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2"""
+    return sum_of_squares(subtract(v, w))
+
+def distance(v: Vector, w: Vector) -> float:
+    """Computes the distance between v and w"""
+    return math.sqrt(squared_distance(v, w))
+
+def scalar_multiply(c: float, v: Vector) -> Vector:
+    """Multiplies every element by c"""
+    return [c * v_i for v_i in v]
+
+def vector_sum(vectors: List[Vector]) -> Vector:
+    """Sums all corresponding elements"""
+    # Check that vectors is not empty
+    assert vectors, "no vectors provided!"
+
+    # Check the vectors are all the same size
+    num_elements = len(vectors[0])
+    assert all(len(v) == num_elements for v in vectors), "different sizes!"
+
+    # the i-th element of the result is the sum of every vector[i]
+    return [sum(vector[i] for vector in vectors)
+            for i in range(num_elements)]
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+    """Computes the element-wise average"""
+    n = len(vectors)
+    return scalar_multiply(1/n, vector_sum(vectors))
+
+def magnitude(v: Vector) -> float:
+    """Returns the magnitude (or length) of v"""
+    return math.sqrt(sum_of_squares(v))   # math.sqrt is square root function
+
+def add(v: Vector, w: Vector) -> Vector:
+    """Adds corresponding elements"""
+    assert len(v) == len(w), "vectors must be the same length"
+
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
